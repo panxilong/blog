ed9fdd966993b8489d3560230cbc58d7035b8483
panxilong <panxilong@kylinos.cn>
2022-01-17 10:31:21 +0800

    deploy

---
+++ assets/umijs.html.37f488d9.js
@@ -0,0 +1,1 @@
+const t={key:"v-5b27d238",path:"/standard/umijs.html",title:"Umijs \u76EE\u5F55",lang:"en-US",frontmatter:{sidebar_position:7},excerpt:"",headers:[],git:{updatedTime:1641974047e3,contributors:[{name:"panxilong",email:"panxilong@kylinos.cn",commits:1}]},filePathRelative:"standard/umijs.md"};export{t as data};
